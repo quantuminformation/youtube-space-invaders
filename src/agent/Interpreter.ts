50ad10a7b4d0da2a40dbf519d8ed99e9264876b0
Nikos Katsikanis <nkatsikanis@gmail.com>
2018-07-27 14:03:54 +0100

    some prettying and interpreter start

--- src/agent/Interpreter.ts
+++ src/agent/Interpreter.ts
@@ -2,18 +2,43 @@
  * Created by nikos on 27/07/2018.
  */
 export class Interpreter {
+  public readPixels() {
+    const gameCanvas: HTMLCanvasElement = document.querySelector(
+      "#game-canvas"
+    ) as HTMLCanvasElement;
+    const miniColourCanvas: HTMLCanvasElement = document.querySelector(
+      "#mini-colour-canvas"
+    ) as HTMLCanvasElement;
+    const monoCanvas: HTMLCanvasElement = document.querySelector(
+      "#mono-canvas"
+    ) as HTMLCanvasElement;
 
-  constructor () {
+    const miniColourCanvasCtx = miniColourCanvas.getContext("2d");
+    const monoCanvasCtx = monoCanvas.getContext("2d");
 
-  }
-
-  public readPixels () {
-    const gameCanvas: HTMLCanvasElement = document.querySelector('#game-canvas') as HTMLCanvasElement
-    const miniColourCanvas: HTMLCanvasElement = document.querySelector('#mini-colour-canvas') as HTMLCanvasElement
-    const monoCanvas: HTMLCanvasElement = document.querySelector('#mono-canvas') as HTMLCanvasElement
-
-    const miniColourCanvasCtx = miniColourCanvas.getContext('2d')
     miniColourCanvasCtx.drawImage(gameCanvas, 0, 0, 100, 100);
 
+    // convert to black and white
+
+    const imageData = miniColourCanvasCtx.getImageData(
+      0,
+      0,
+      miniColourCanvas.width,
+      miniColourCanvas.height
+    );
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+      const brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+      // red
+      data[i] = brightness;
+      // green
+      data[i + 1] = brightness;
+      // blue
+      data[i + 2] = brightness;
+    }
+
+    // overwrite original image
+    monoCanvasCtx.putImageData(imageData, 0, 0);
   }
 }
